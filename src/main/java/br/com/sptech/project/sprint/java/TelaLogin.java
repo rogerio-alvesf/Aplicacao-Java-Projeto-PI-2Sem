67a5153176eca594a1147d132609b6637cfe2c8e
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-05-19 19:03:45 -0300

    Subindo alterações referentes ao nome da classe telaLogin

--- src/main/java/br/com/sptech/project/sprint/java/TelaLogin.java
+++ src/main/java/br/com/sptech/project/sprint/java/TelaLogin.java
@@ -4,11 +4,11 @@ import static br.com.sptech.project.sprint.java.App.ColetarDados;
 import java.awt.Color;
 import models.Login;
 
-public class Main extends javax.swing.JFrame {
+public class TelaLogin extends javax.swing.JFrame {
     
     Maquina maquinaAcessada = new Maquina();
     
-    public Main() {
+    public TelaLogin() {
         initComponents();
     }
 
@@ -210,14 +210,18 @@ public class Main extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -226,7 +230,7 @@ public class Main extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                Main telaLogin = new Main();
+                TelaLogin telaLogin = new TelaLogin();
                 telaLogin.setExtendedState(6);
                 telaLogin.setVisible(true);
             }
